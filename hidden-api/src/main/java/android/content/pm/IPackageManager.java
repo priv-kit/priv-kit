e667c5689166927b58be92e5d895df30e53e3d95
二刺螈 <i@songe.li>
2026-07-26 02:27:12 +0800

    chore: update hidden-api

--- hidden-api/src/main/java/android/content/pm/IPackageManager.java
+++ hidden-api/src/main/java/android/content/pm/IPackageManager.java
@@ -8,13 +8,13 @@ import android.os.IInterface;
  * @noinspection unused
  */
 public interface IPackageManager extends IInterface {
-    int checkPermission(String permName, String pkgName, int userId);
-
-    void grantRuntimePermission(String packageName, String permissionName, int userId);
-
     abstract class Stub extends Binder implements IPackageManager {
         public static IPackageManager asInterface(IBinder obj) {
             throw new RuntimeException();
         }
     }
+
+    int checkPermission(String permName, String pkgName, int userId);
+
+    void grantRuntimePermission(String packageName, String permissionName, int userId);
 }
--- hidden-api/src/main/java/android/os/IUserManager.java
+++ hidden-api/src/main/java/android/os/IUserManager.java
@@ -14,7 +14,9 @@ public interface IUserManager extends IInterface {
         }
     }
 
+    // 9 - 10.0.0_r29, 10.0.0_r46 - 10, 16.0.0_r3 - 17
     List<UserInfo> getUsers(boolean excludeDying);
 
+    // 10.0.0_r30 - 10.0.0_r45, 11 - 16.0.0_r2
     List<UserInfo> getUsers(boolean excludePartial, boolean excludeDying, boolean excludePreCreated);
 }
